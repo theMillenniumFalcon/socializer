c1992ef1237b4653c688b70079be68cb0ef8b924
theMillenniumFalcon <nishankpr2002@gmail.com>
2023-01-26 23:54:36 +0530

    added notification screen

---
+++ app/src/screens/notification/NotiIndex.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { SafeAreaView, Text } from "react-native"
+
+const NotiIndex = () => {
+    return (
+        <SafeAreaView>
+            <Text>Search</Text>
+        </SafeAreaView>
+    )
+}
+
+export default NotiIndex
